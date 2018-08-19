618cb274f357629cac75f497d84739f7de85b052
mattseneque <mattseneque@gmail.com>
2018-08-19 22:09:24 +0800

    Adds demo chart area percentage

--- Vidly/Controllers/HomeController.cs
+++ Vidly/Controllers/HomeController.cs
@@ -1,16 +1,66 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Vidly.ViewModels;
+using Vidly.Models;
 
 namespace Vidly.Controllers
 {
+   
+
+    public static class JavaScriptConvert
+    {
+        public static IHtmlString SerializeObject(object value)
+        {
+            using (var stringWriter = new StringWriter())
+            using (var jsonWriter = new JsonTextWriter(stringWriter))
+            {
+                var serializer = new JsonSerializer
+                {
+                    // Let's use camelCasing as is common practice in JavaScript
+                    ContractResolver = new CamelCasePropertyNamesContractResolver()
+                };
+
+                // We don't want quotes around object names
+                jsonWriter.QuoteName = false;
+                serializer.Serialize(jsonWriter, value);
+
+                return new HtmlString(stringWriter.ToString());
+            }
+        }
+    }
+
+
     public class HomeController : Controller
     {
         public ActionResult Index()
         {
-            return View();
+            var allChartData = new List<ChartData> { };
+
+            var c1 = new ChartData
+            {
+                name = "Asia",
+                data = new List<int>{ 502, 635, 809, 947, 1402, 3634, 5268 }
+            };
+            
+            allChartData.Add(c1);
+            allChartData.Add(new ChartData
+            {
+                name = "Africa",
+                data = new List<int> { 106, 107, 111, 133, 221, 767, 1766 }
+            });
+
+            var viewModel = new HomeIndexViewModel
+            {
+                ChartDatas = allChartData
+            };
+
+            return View(viewModel);
         }
 
         public ActionResult About()
---
+++ Vidly/Models/ChartData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Vidly.Models
+{
+    public class ChartData
+    {
+        public string name { get; set; }
+        public List<int> data { get; set; }
+    }
+}
